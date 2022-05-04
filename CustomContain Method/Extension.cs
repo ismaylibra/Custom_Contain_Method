c02cf5277768079fb6a23416bf16ead618460169
ismaylibra <ismayiliib@code.edu.az>
2022-05-04 16:24:17 +0400

    added custom contain method

---
+++ CustomContain Method/Extension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomContain_Method
+{
+    static class Extension
+    {
+      public static void CustomContain(this string sentence, string search)
+        {
+            bool result = false;
+            string Check = string.Empty;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if ((i + search.Length) > sentence.Length)
+                {
+                    break;
+                }
+                Check = sentence.Substring(i, search.Length);
+                if (Check.ToLower() == search.ToLower())
+                {
+                    result = true;
+                    break;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ CustomContain Method/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomContain_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cumle daxil edin: ");
+            string sentence = Console.ReadLine();
+            Console.Write("Axtardiginiz hisseni daxil edin: ");
+            string search = Console.ReadLine();
+            Console.Write("Netice: ");
+            sentence.CustomContain(search);
+            
+        }
+    }
+}
